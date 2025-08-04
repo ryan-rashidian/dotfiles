53f4b7bf72493c7e8d23acc3ba11aa1e11abf30d
ryan-rashidian <ryanrashidian@gmail.com>
2025-08-03 18:51:30 -0700

    Add setup script for Arch/Hyprland post-pacstrap package installations

---
+++ setup.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+#
+# Arch Linux + Hyprland post-bootstrap setup script.
+#
+set -e # stop on error
+
+sudo pacman -Syu --noconfirm
+sudo pacman -S --needed --noconfirm base-devel git
+
+if ! command -v paru &> /dev/null; then
+    echo "Installing Paru AUR..."
+    mkdir -p ~/tmp
+    git clone https://aur.archlinux.org/paru.git ~/tmp/paru
+    cd ~/tmp/paru
+    makepkg -si --noconfirm
+    cd ~
+    rm -rf ~/tmp/paru
+fi
+
+paru -Syu --noconfirm
+
+# Essentials
+echo "Installing essentials..."
+paru -S --noconfirm \
+    htop \
+    tmux \
+    openssh \
+    unzip \
+    curl \
+    wget \
+    tree \
+    kitty \
+    ghostty \
+    neovim \
+    firefox \
+    nwg-look \
+    thunar \
+    npm \
+    nodejs \
+    clang
+
+# Extras
+echo "Installing extras..."
+paru -S --noconfirm \
+    ttf-firacode \
+    ttf-firacode-nerd \
+    adw-gtk-theme \
+    obs-studio \
+    kicad
+
+# Hyprland
+echo "Installing Hyprland..."
+paru -S --noconfirm \
+    hyprland \
+    hyprpaper \
+    waybar \
+    wofi \
+    wl-clipboard \
+    xdg-desktop-portal-hyprland \
+    pavucontrol \
+    udiskie \
+    zathura \
+    imv \
+    mpv
+
+xdg-mime default imv.desktop image/png
+xdg-mime default imv.desktop image/jpeg
+xdg-mime default zathura.desktop application/pdf
+xdg-mime default mpv.desktop video/x-matroska
+xdg-mime default mpv.desktop video/webm
+xdg-mime default mpv.desktop video/mp4
+
+# Firewall
+# - Basic firewalld configuration
+paru -S --noconfirm firewalld
+sudo systemctl enable firewalld --now
+
+echo "Bootstrap Complete."
+
